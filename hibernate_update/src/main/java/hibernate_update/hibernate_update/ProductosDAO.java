2fd29fe26af648a7f6ff2fa7bc92ae55c454fa0c
lastlightR <robyn_ng@outlook.es>
2024-02-27 15:08:25 +0100

    Actualización con nuevos métodos DAO

--- hibernate_update/src/main/java/hibernate_update/hibernate_update/ProductosDAO.java
+++ hibernate_update/src/main/java/hibernate_update/hibernate_update/ProductosDAO.java
@@ -1,5 +1,8 @@
 package hibernate_update.hibernate_update;
 
+import java.util.Iterator;
+import java.util.List;
+
 import jakarta.persistence.*;
 import jakarta.transaction.*;
 
@@ -9,17 +12,19 @@ public class ProductosDAO {
 	@PersistenceContext
 	private EntityManager entityManager;
 	
-	//construct that takes the entity manager
+	//constructor that takes the entity manager
 	public ProductosDAO(EntityManager em) {
 		this.entityManager = em;
 	}
 	
+	//method that inserts a new product in the table
 	public void insertProducto(Productos producto) {
 		entityManager.persist(producto);
 		System.out.println("Added new product with name: " + producto.getNombre());
 		System.out.println("====================================================");
 	}
 	
+	//method that removes a record from the table based on its ID
 	public void deleteProducto(int id_producto) {
 		Productos producto = entityManager.find(Productos.class, id_producto);
 		if (producto != null) {
@@ -27,6 +32,39 @@ public class ProductosDAO {
 			entityManager.remove(producto);
 		}
 		else System.out.println("Could not find a product with that ID");	
+		System.out.println("====================================================");
+	}
+	
+	//method that receives an ID of a product and the new name and price to assign
+	public void updateProducto(int id_producto, String nombre, double precio) {
+		Productos producto = entityManager.find(Productos.class, id_producto);
+		if (producto != null) { //updates only if it finds the ID
+			System.out.println("Found product with ID: " + id_producto + ", Name: " + producto.getNombre() + ", Price: " + producto.getPrecio());
+			System.out.println("====================================================");
+			producto.setNombre(nombre); producto.setPrecio(precio);
+			System.out.println("Updating to Name: " + nombre + ", Price: " + precio);
+			System.out.println("====================================================");
+			entityManager.merge(producto);
+			System.out.println("Product updated");
+		}
+		else System.out.println("Could not find a product with that ID");
+		System.out.println("====================================================");
+	}
+	
+	//method that shows all products on the table on screen
+	public void showProducts() {
+		List all_products = entityManager.createQuery("FROM Productos").getResultList();
+		Iterator iterator = all_products.iterator();
+		System.out.println("Looking for products...");
+		System.out.println("====================================================");
+		
+		while(iterator.hasNext()) { //showing products row by row
+			Productos producto = (Productos) iterator.next();
+			System.out.println("ID: " + producto.getId() + "\tName: " + producto.getNombre()
+			+ "\tPrice: " + producto.getPrecio());
+			System.out.println("====================================================");
+		}
+		System.out.println("Finished searching for products");
 		System.out.println("====================================================");
 	}
 }
--- hibernate_update/src/main/java/hibernate_update/hibernate_update/UseHibernate.java
+++ hibernate_update/src/main/java/hibernate_update/hibernate_update/UseHibernate.java
@@ -6,24 +6,36 @@ public class UseHibernate
 {
     public static void main( String[] args )
     {
-    	//add name of the persistence unit when creating an EntityManagerFactory
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnit");
+    	try {
+    		//add name of the persistence unit when creating an EntityManagerFactory
+    		EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnit");
         
-        EntityManager em = emf.createEntityManager();
+    		EntityManager em = emf.createEntityManager();
         
-        //new ProductosDAO
-        ProductosDAO dao = new ProductosDAO(em);
+    		//new ProductosDAO
+    		ProductosDAO dao = new ProductosDAO(em);
         
-        //new producto
-        Productos producto = new Productos("MediaMarkt RAM", 500);
+    		//new producto
+    		Productos producto = new Productos("MediaMarkt RAM", 500);
         
-        //insert product in database
-        dao.insertProducto(producto);
+    		//insert product in database
+    		dao.insertProducto(producto);
         
-        //delete product from the database
-        dao.deleteProducto(11);
+    		//delete product from the database
+    		dao.deleteProducto(11);
+    		
+    		//update product
+    		dao.updateProducto(5, "Software guay", 250);
+    		
+    		//show products in list
+    		dao.showProducts();
         
-        em.close();
-        emf.close();
+    		em.close();
+    		emf.close();
+    	}
+    	catch(Exception ex) {
+    		System.out.println("Error in main class: " +ex);
+    		ex.printStackTrace();
+    	}
     }
 }
